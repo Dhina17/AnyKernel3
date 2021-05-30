3d307ddfd693d80214253dcf0c23ad409311dc38
Dhina17 <dhinalogu@gmail.com>
2021-05-30 21:03:21 +0530

    anykernel: Update kernel.string

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=KernelNoob by Dhina_17 @ xda-developers
+kernel.string=Test Kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
